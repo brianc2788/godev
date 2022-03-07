f9b93ef4ef09092fd762921323fc0a58d3527634
brian2788 <userone@gl7510sdk>
2022-03-07 01:21:47 -0500

    Minor edits to 'hellogo' project.

--- hellogo/hellogo.go
+++ hellogo/hellogo.go
@@ -3,7 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Ken Thompson!\n",
+	fmt.Println("\n",					//prefixing a newline for output uniformity.
+			"Hello, Ken Thompson!\n",
 		    "I mean,\n",
 		    "Hello, GO!")
 }
