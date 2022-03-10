fac90e7ffd93c3343071567509ff1ae04f427164
brian2788 <userone@gl7510sdk>
2022-03-10 00:54:40 -0500

    Added 'net/portscan1'. Scans 'scanme.nmap.org' http port.

---
+++ netpkg/portscan1/portscan1.go
@@ -0,0 +1,20 @@
+/*
+use net module to create 
+*/
+package main
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	// sock stream (tcp) or sock dgram (udp).
+	// host is host domain & port.
+	var socket = "tcp"
+	var host = "scanme.nmap.org:80"
+
+	_, err := net.Dial(socket,host)
+	if err == nil {
+		fmt.Println("Connected to",host)	// sep defaults to ' '.
+	}
+}
