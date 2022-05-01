5ab4e3166e6cb1998fdc8a589333ba463cd4980d
brianc2788 <brianc2788@gmail.com>
2022-05-01 14:52:40 -0400

    Reorganization of multiple files in the repo.

--- hellogo/hellogo.go
+++ hellogo/hellogo.go
@@ -3,8 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("\n",					//prefixing a newline for output uniformity.
-			"Hello, Ken Thompson!\n",
-		    "I mean,\n",
-		    "Hello, GO!")
+	// declare some local variables.
+	worldstr := "world"
+	UltimateHuman := "Ken Thompson"
+	var1,var2 := "thing1","thing2"
+	
+	//bunches of ways to print stuff...
+	//every beginner tutorial wants to use Println for some reason.
+	//however, there's apparently a print function for any situation...
+	fmt.Println("Hello,", UltimateHuman, "!") //note: fmt inserts a default seperator.
+	fmt.Printf("I mean, jello, %s!\n", worldstr)
+	fmt.Println("I mean...\nHello, Println, Printf, Print, et al.")
+	fmt.Print("Print() just prints the stuff you give it; no separators, formatting, etc.\nsee? no newline.")
+	fmt.Printf("\nmore debug:\n%s - %s\n",var1,var2)
+	fmt.Print("Exiting.......")
 }
