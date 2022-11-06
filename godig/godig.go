995b23706f294ee740851635e16c327a6870daaf
brianc2788 <brianc2788@gmail.com>
2022-11-05 22:00:48 -0400

    Added arg input, wrapped dns exchange in a function.

--- godig/godig.go
+++ godig/godig.go
@@ -1,6 +1,6 @@
 /***********************************************************
  * godig
- *
+ * -----
  * A version of the dig tool from gnu. Sort of.
  *
  http://brianc2788.github.io
@@ -8,12 +8,23 @@
 package main
 import (
 	"fmt"
+	"os"
 	"github.com/miekg/dns"
 )
 
-func main() {
-	var msg dns.MSG
-	fqdn := Fqdn("stacktitan.com")
+func sendQuery(domainName string) {
+	var msg dns.Msg
+	fqdn := dns.Fqdn(domainName)
 	msg.SetQuestion(fqdn,dns.TypeA)
 	dns.Exchange(&msg, "8.8.8.8:53")
+	fmt.Println(msg)
+}
+
+func main() {
+	// Loop through args and query each.
+	// Print output (still need to decompress, parse, etc.)
+	// Answers can be seen via tcpdump/packet capture.
+	for i := 1; i < len(os.Args); i++ {
+		sendQuery(os.Args[i])
+	}
 }
