2dab0da4ce6c33ae4ecc4f8f7b731b0125a93b9f
brianc2788 <brianc2788@gmail.com>
2022-11-06 14:43:34 -0500

    Big commit. getDnsAddr\(\), global constants, etc.

--- godig/godig.go
+++ godig/godig.go
@@ -2,7 +2,14 @@
  * godig
  * -----
  * A version of the dig tool from gnu. Sort of.
- *
+ * Inspired by "Black Hat Go" by Tom Steele, Chris Patten, and Dan Kottmann.
+ * 
+ * TODO:-Parse and, if necessary, decompress dns response.
+ * 		-Impl. arg parser with switches for options.
+ * 		-Opts include dns server, query type, etc.
+ * 		-Impl. other dns query types, services.
+ * 		-DNSec?
+ * 		-Continue improving code readability & optimize.
  http://brianc2788.github.io
 ***********************************************************/
 package main
@@ -12,19 +19,35 @@ import (
 	"github.com/miekg/dns"
 )
 
-func sendQuery(domainName string) {
+/* Constants - Port & DNS Server Addresses (ipv4). */
+const DNS_SEPCHAR = ":"
+const DNS_PORT = "53"
+const DNS_GOOGLE_ADDR = "8.8.8.8"
+const DNS_CLOUDFLARE_ADDR = "1.1.1.1"
+const DNS_LOCAL_ADDR = "192.168.1.1"
+
+func getDnsAddr() string {
+	fullAddr := DNS_GOOGLE_ADDR + DNS_SEPCHAR + DNS_PORT
+	return fullAddr
+}
+
+/* Resolve names via DNS, print to stdout. */
+func sendQuery(domainName string) dns.Msg {
 	var msg dns.Msg
 	fqdn := dns.Fqdn(domainName)
 	msg.SetQuestion(fqdn,dns.TypeA)
-	dns.Exchange(&msg, "8.8.8.8:53")
-	fmt.Println(msg)
+	dns.Exchange(&msg, getDnsAddr())
+	
+	return msg
 }
 
 func main() {
-	// Loop through args and query each.
-	// Print output (still need to decompress, parse, etc.)
-	// Answers can be seen via tcpdump/packet capture.
-	for i := 1; i < len(os.Args); i++ {
-		sendQuery(os.Args[i])
+	argList := os.Args
+	argCount := len(argList)
+
+	/* Loop through args with sendQuery(). */
+	for userArg := 1; userArg < argCount; userArg++ {
+		response := sendQuery(argList[userArg])
+		fmt.Printf("%s is at %s\n", argList[userArg], response)
 	}
 }
