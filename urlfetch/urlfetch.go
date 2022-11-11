062a5c656ba9af2d63ea975954cf4c8cbbde9557
brianc2788 <brianc2788@gmail.com>
2022-11-10 23:04:43 -0500

    Edits to description in initial block-comment.

--- urlfetch/urlfetch.go
+++ urlfetch/urlfetch.go
@@ -1,35 +1,35 @@
 /*******************************************************************************
  * urlfetch.go
  * -----------
- * combining chapters 1.2, 1.5 - cli args, fetching urls.
- * Right now, just takes a URL (with schema) and returns the html.
+ * Combining chapters 1.2, 1.5 of The Go Programming Lang. - cli args, fetching urls.
+ * Right now, just takes a URL and returns the html.
  * http://brianc2788.github.io
 *******************************************************************************/
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 func main() {
-	for _,url := range os.Args[1:] {
-        resp,err := http.Get(url)
-        
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "fetch error: %v\n", err)
-            os.Exit(1)
-        }
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
 
-        b,err := ioutil.ReadAll(resp.Body)
-        resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch error: %v\n", err)
+			os.Exit(1)
+		}
 
-        if err != nil {
-            fmt.Fprintf(os.Stderr, "fetch read: %s (%v)\n", url, err)
-            os.Exit(1)
-        }
-        fmt.Printf("%s", b)
-    }
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch read: %s (%v)\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
 }
