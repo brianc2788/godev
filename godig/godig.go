b36bfb2ea5593f0ae4787c1cd2fb28a06ec3f0bc
brianc2788 <brianc2788@gmail.com>
2022-11-25 00:16:54 -0500

    Added TODO - PTR questions/reverse dns comment w/func.

--- godig/godig.go
+++ godig/godig.go
@@ -25,26 +25,39 @@ import (
 )
 
 /* Constants - Port & DNS Server Addresses (ipv4). */
-const DNS_SEPCHAR = ":"
+const DNS_SEP = ":"
 const DNS_PORT = "53"
 const DNS_GOOGLE_ADDR = "8.8.8.8"
 const DNS_CLOUDFLARE_ADDR = "1.1.1.1"
 const DNS_LOCAL_ADDR = "192.168.1.1"
 
 func getDnsAddr() string {
-	fullAddr := DNS_GOOGLE_ADDR + DNS_SEPCHAR + DNS_PORT
+	fullAddr := DNS_GOOGLE_ADDR + DNS_SEP + DNS_PORT
 	return fullAddr
 }
 
 /* Resolve names via DNS, print to stdout. */
 func sendQuery(domainName string) dns.Msg {
 	var msg dns.Msg
-	fqdn := dns.Fqdn(domainName)
+	fqdn := dns.Fqdn(domainName) // Don't need trailing period.
 	msg.SetQuestion(fqdn, dns.TypeA)
 	dns.Exchange(&msg, getDnsAddr())
 
 	return msg
 }
+
+/* PTR Record queries. Not yet implemented.
+------------------------------------------------------------
+   dig example:
+   (Querying ipv4 address 94.177.245.177)
+   user@localhost$ dig 177.245.177.94.in-addr.arpa PTR
+   Note: ipv6 format: [addr].ipv6.arpa
+------------------------------------------------------------
+
+func getRecordPTR(address string) dns.Msg {
+	//TODO: Reverse DNS lookup.
+}
+*/
 
 func main() {
 	argList := os.Args
@@ -53,6 +66,6 @@ func main() {
 	/* Loop through args with sendQuery(). */
 	for userArg := 1; userArg < argCount; userArg++ {
 		response := sendQuery(argList[userArg])
-		fmt.Printf("%s is at %s\n", argList[userArg], response)
+		fmt.Printf("%s is at %s\n", argList[userArg], response.Answer)
 	}
 }
