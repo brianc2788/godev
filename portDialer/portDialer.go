5adcbc308aef864164be819c6fd6604e6f6c5999
brianc2788 <brianc2788@gmail.com>
2022-12-10 11:09:12 -0500

    Fixed goroutines with sync.

--- portDialer/portDialer.go
+++ portDialer/portDialer.go
@@ -1,12 +1,15 @@
 /*
+********************************************************************************
 portDialer version 2.1.0
 ------------------------
-Added concurrent goroutines.
-TODO: Parse args for ports and ranges.
+Concurrent port scanner. Open port indicates completion of a TCP connection(!).
+TODO: -Print help/usage.
 
-	Improve concurrent subroutines.
+	      -Parse args for ports; comma-seperated & hyphenated-range formats.
+		  -Emulate a nmap stealth scan by not completing TCP 3-way handshake (?).
 
 http://brianc2788.github.io/
+********************************************************************************
 */
 package main
 
@@ -14,47 +17,50 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"sync"
 	"time"
 )
 
 func main() {
 	args := os.Args
 	if len(args) < 3 {
-		fmt.Printf("Need domain & port(s)\n")
+		fmt.Printf("Enter domain name & white-space seperated port(s).\n")
 		os.Exit(0)
 	}
 	dname := args[1]
+	var wgroups sync.WaitGroup
 
 	go spinner()
 
 	// Loop through args[2:] (ports)
 	for n := 2; n < len(args); n++ {
-		go checkPort(dname, args[n])
+		wgroups.Add(1)
+		go checkPort(dname, args[n], &wgroups)
 	}
 
-	// wait for goroutines
-	time.Sleep(11 * time.Second)
+	wgroups.Wait()
 }
 
 func spinner() {
 	for {
 		for _, r := range `-\|/` {
-			fmt.Printf("%c\r", r)
+			fmt.Printf("*Scanning [%c]\r", r)
 			time.Sleep(100 * time.Millisecond)
 		}
 	}
 }
 
-func checkPort(a string, p string) {
+func checkPort(a string, p string, j *sync.WaitGroup) {
 	to, terr := time.ParseDuration("10s")
+	defer j.Done()
 	if terr != nil {
 		panic(terr)
 	}
 	c, err := net.DialTimeout("tcp", a+":"+p, to)
 	if err != nil {
-		fmt.Printf("%s - port %s filtered|closed\n", a, p)
+		fmt.Printf("\r%s - port %s filtered|closed\n", a, p)
 	} else {
-		fmt.Printf("%s - port %s open\n", a, p)
+		fmt.Printf("\r%s - port %s open\n", a, p)
 		/* Printed out ipv4, but didn't come out right. Move somewhere else?
 		ip4 := c.RemoteAddr().String()
 		ip4 = ip4[:((len(ip4)-len(p))-1)]
